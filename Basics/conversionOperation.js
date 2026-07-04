c347b46da9cf2b7234cb4cd78de5f1336e6e64bb
VanshPatel1496 <vanshpatel1496@gmail.com>
2026-07-04 16:22:12 +0000

    conversion of datatypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,54 @@
+/*let score = "33"
+
+console.log(typeof score); //string
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);// 33 */
+
+/*let score = "33abc"
+
+console.log(typeof score); //string
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);//NaN*/
+
+
+/*let score = null
+
+console.log(typeof score); //object
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);//0*/
+
+
+/*let score = undefined
+
+console.log(typeof score); //undefined
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);//NaN*/
+
+
+/*let score = true
+
+console.log(typeof score); //boolean
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);//1*/
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isloggedIn = 1
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vansh" => true
