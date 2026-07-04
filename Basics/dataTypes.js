d588da760fef5293e3fc56dfcfe31751ef500e4f
VanshPatel1496 <vanshpatel1496@gmail.com>
2026-07-04 12:06:14 +0000

    Data types

---
+++ Basics/dataTypes.js
@@ -0,0 +1,13 @@
+let name = "Vivek"
+let age = 30
+let isLoggedIn = false
+// string => ""
+// boolean => true/false    
+// null => standalone value that represents nothingness
+// undefined => variable that has been declared but not assigned a value
+// symbol => unique and immutable primitive value
+
+// object => collection of properties
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
