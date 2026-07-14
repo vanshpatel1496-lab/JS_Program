2699008c5aec2e058c2e442ac839412414fea722
VanshPatel1496 <vanshpatel1496@gmail.com>
2026-07-14 04:54:18 +0000

    string manipulation

---
+++ Basics/strings.js
@@ -0,0 +1,9 @@
+const name = "vansh"
+const repoCount = 50 
+
+//console.log(name + repoCount + "Value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new  String('vanshpp')
+
